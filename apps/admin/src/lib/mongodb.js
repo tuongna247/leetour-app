dad4eb26870fbbc93a5571167e023de5dfe4dcbb
Tuong Nguyenanh <tuong.nguyenanh@elinext.com>
2025-11-01 21:15:12 +0700

    Upload to production

--- apps/admin/src/lib/mongodb.js
+++ apps/admin/src/lib/mongodb.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = 'mongodb+srv://leetour:RN1vmYdHHjnTwEqM@cluster0.nz7bupo.mongodb.net/leetour?retryWrites=true&w=majority&appName=Cluster0';
+const MONGODB_URI = process.env.MONGODB_URI || process.env.NEXT_PUBLIC_MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+}
 
 /**
  * Global is used here to maintain a cached connection across hot reloads
